e08ec75d5618bb6f79be5a57004568b486540d2f
hironamo <tsh21537@gmail.com>
2026-05-19 16:04:28 +0900

    Add files via upload

---
+++ gemini-code-1779174218749.js
@@ -0,0 +1,21 @@
+// sw.js
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+// バックグラウンド通知を受信・表示する処理
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+            if (clientList.length > 0) {
+                return clientList[0].focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
+});
